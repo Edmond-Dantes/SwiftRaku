839d4dc0bf6ba9fae501cc84c19a45d9901a6797
Edmond Akpan <edmond.akpan@gmail.com>
2019-05-26 18:08:06 +0900

    Initial Commit

---
+++ SwiftRaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftRaku
+//
+//  Created by Edmond on 2019/05/26.
+//  Copyright © 2019 Edmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftRakuTests/SwiftRakuTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftRakuTests.swift
+//  SwiftRakuTests
+//
+//  Created by Edmond on 2019/05/26.
+//  Copyright © 2019 Edmond. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftRaku
+
+class SwiftRakuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
